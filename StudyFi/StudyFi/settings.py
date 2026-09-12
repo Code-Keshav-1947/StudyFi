81d3d4b3b99d7932e280acefcbbae727c7f67565
Keshav <manudeepaksharma2012@gmail.com>
2026-09-12 21:13:23 +0530

    Add database configuration for production and local development in settings.py

--- StudyFi/StudyFi/settings.py
+++ StudyFi/StudyFi/settings.py
@@ -10,10 +10,32 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.1/ref/settings/
 """
 
+import os
 from pathlib import Path
+
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# DataBase Path
+DATABASE_URL = os.environ.get("DATABASE_URL")
+
+if DATABASE_URL:
+    # 🚀 PRODUCTION (Render + Supabase)
+    DATABASES = {
+        "default": dj_database_url.config(
+            default=DATABASE_URL, conn_max_age=600, ssl_require=True
+        )
+    }
+else:
+    # 💻 LOCAL DEVELOPMENT (SQLite)
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 
 # Quick-start development settings - unsuitable for production
@@ -81,15 +103,6 @@ REST_FRAMEWORK = {
     ],
 }
 
-# Database
-# https://docs.djangoproject.com/en/6.1/ref/settings/#databases
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
 
 
 # Password validation
